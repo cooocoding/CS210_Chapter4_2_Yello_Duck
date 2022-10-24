a8b262ddf675d22e247eb68e53ceb911c6c825af
Chen Rui <chenrui666ok@126.com>
2022-10-24 19:36:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lecture4_2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lecture4_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
